ec926daa140273ab9cfda3b0b844f6e340ceb651
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-11-14 15:52:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+/* for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+} */
+
+/* const gifts =["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrap ${gifts[i]} and added a bowl!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts); */
+
+
+function writeCards(stringName, eventName) {
+    let result =[];
+    for (let i = 0; i < stringName.length; i++) {
+        result.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return result;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    return number;
+} 
+
+countDown(10);
+
+
